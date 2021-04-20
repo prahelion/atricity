2fce4b4d13bb1edfe150f51e0246f8ad245abde1
parhelion <prahelion@prahelion.xyz>
2021-04-20 12:13:46 -0400

    added stuff

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// TEMP
+// Imports
+const slash = require('./resources/modules/slash.js');
+const discord = require('discord.js');
+const fs = require('fs');
+//
+
+// Variables
+const client = new discord.Client();
+
+// Setup
+require('dotenv').config();
+client.login(process.env.TOKEN);
+slash.authenticate(process.env.TOKEN);
+//
+
+// Start
+    
+//
